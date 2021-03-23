e230a74bb123a802597ca1c604a8e03cfdb59908
chaultm29 <chaultm29@gmail.com>
2021-03-23 12:32:48 +0700

    Init Project

---
+++ LifeSimulator/app/src/main/java/com/example/lifesimulator/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.lifesimulator;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+import com.example.lifesimulator.Fragment.*;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+    Fragment selectedFragment = null;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+
+        bottomNavigationView.setOnNavigationItemSelectedListener(navigationItemSelectedListener);
+        bottomNavigationView.setItemIconTintList(null);
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new IdentityFragment()).commit();
+
+    }
+private BottomNavigationView.OnNavigationItemSelectedListener navigationItemSelectedListener = new BottomNavigationView.OnNavigationItemSelectedListener() {
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_identity:
+                selectedFragment = new IdentityFragment();
+                break;
+            case R.id.nav_relation:
+                selectedFragment = new RelationFragment();
+                break;
+            case R.id.nav_job:
+                selectedFragment = new JobFragment();
+                break;
+            case R.id.nav_cure:
+                selectedFragment = new CureFragment();
+                break;
+            case R.id.nav_bank:
+                selectedFragment = new BankFragment();
+                break;
+        }
+        if (selectedFragment != null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+        }
+        return true;
+    }
+
+};
+
+
+}
